316f3e2f0381e828d9ccaa1665215063a5b8fc6c
RyanAsh93 <blueheadryan93@yahoo.com>
2020-05-14 11:08:05 -0600

    stubed out comments for project

--- main.rb
+++ main.rb
@@ -1,1 +1,9 @@
-#make a change
+students = ["Leveille, Andre", "AshWorth, Ryan", "Biddinger, Caden", "Mbia, Arsene", "Boyden, Lindsay", "DelaCruz, Joey", "Dunshee, Kyle", "Riches, James", "Smith, Shawn", "Sultan, Maxwell", "Zarate, Javier"]
+
+# task now is to create a group randomizer
+
+# create a new array of hashes with key first_name and last_name
+
+# with the newly created array i want to randomly select people and pull and assign
+
+# print out groups in a slack friendly format
